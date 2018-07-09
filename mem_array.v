37c00e02500695b1af258f47dbbb58387afec61c
AdityaPawar5 <apbr5@mst.edu>
2018-07-09 13:48:57 -0700

    Update mem_array.v

--- mem_array.v
+++ mem_array.v
@@ -6,12 +6,14 @@
 // Place: Cadence Design Systems, Inc.
 // Date: July 10, 2008
 //***********************************************************//
-```
+
 module mem_array #(parameter ADDR_WIDTH = 4,parameter DEPTH = 16,parameter DATA_WIDTH = 16)
 (
 input [ADDR_WIDTH-1:0] write_addr,input [ADDR_WIDTH-1:0] read_addr,
 input write_enable,input read_enable,input clk,input [DATA_WIDTH-1:0] write_data,
+
 output reg [DATA_WIDTH-1:0] read_data); 
+
 reg [DATA_WIDTH-1:0] mem [0:DEPTH-1];
 
 // Mem write
@@ -28,5 +30,6 @@ begin
 		read_data <= mem[read_addr];
 	end
 end
+
 endmodule
-```
+
